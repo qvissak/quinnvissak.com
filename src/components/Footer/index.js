0a4628ab276d8c407d6cf8c862bdba6badd5f1d0
qvissak <quinn.vissak@uconn.edu>
2018-06-10 01:20:57 -0400

    Implement footer
    Responsive on mobile, tablet and web.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
+import { colors, email } from '../../config'
 import './styles.css'
 
+const styles = { color: colors.white }
+
 class Footer extends React.Component {
-  render () { return <div></div> }
+  shouldComponentUpdate = () => false
+
+  sendEmail () { document.location = 'mailto:' + email }
+
+  makeIcon = (url, name, path) =>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <img className="footer-icons" alt={name} src={path} />
+    </a>
+
+  render = () =>
+    <footer className="footer-container" style={styles}>
+      <div className="footer-item">
+        code &rarr;
+        {this.makeIcon('https://www.github.com/qvissak/quinnvissak.com', 'github', 'icons/github.svg')}
+      </div>
+      <div className="footer-item">Quinn Vissak &copy; 2018</div>
+      <div className="footer-item">
+        <a onClick={() => this.sendEmail()}>
+          <img className="footer-icons" alt="email" src="icons/email.svg" />
+        </a>
+        {this.makeIcon('https://www.facebook.com/quinn.vissak', 'facebook', 'icons/facebook.svg')}
+        {this.makeIcon('https://www.linkedin.com/in/quinn-vissak', 'linkedin', 'icons/linkedin.svg')}
+      </div>
+    </footer>
 }
 
 export default Footer
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const colors = {
+  white: '#d7e2dc'
+}
+
+export const email = "quinn.vissak@uconn.edu"
