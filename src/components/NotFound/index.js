7ab92ae2a749b91b2fc56324763f07da170dcbd4
qvissak <quinn.vissak@uconn.edu>
2018-09-09 22:00:06 -0400

    Add 404 Not Found page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -3,7 +3,11 @@ import React from 'react'
 import './styles.css'
 
 class NotFound extends React.Component {
-  render = () => <div></div>
+  render = () =>
+    <div className="not-found-container">
+      <div>404</div>
+      <div>Sorry, the page {this.props.location.pathname} is elusive.</div>
+    </div>
 }
 
 export default NotFound
